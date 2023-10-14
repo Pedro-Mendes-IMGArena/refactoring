8cbb85e28723ff02817ea60867dd0728a0b9617a
Pedro Mendes <108732350+Pedro-Mendes-IMGArena@users.noreply.github.com>
2023-10-14 10:35:02 +0100

    refactor: inline variable to prepare extraction

--- src/statement.ts
+++ src/statement.ts
@@ -27,14 +27,13 @@ export function statement(invoice: Invoice, plays: Record<string,Play>) {
 
     for (let perf of invoice.performances) {
         const play: Play = plays[perf.playID];
-        let thisAmount = calculateAmountOwedBy(play.type, perf.audience);
         // add volume credits
         volumeCredits += Math.max(perf.audience - 30, 0);
         // add extra credit for every ten comedy attendees
         if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
         // print line for this order
-        result += ` ${play.name}: ${format(thisAmount / 100)} (${perf.audience} seats)\n`;
-        totalAmount += thisAmount;
+        result += ` ${play.name}: ${format(calculateAmountOwedBy(play.type, perf.audience) / 100)} (${perf.audience} seats)\n`;
+        totalAmount += calculateAmountOwedBy(play.type, perf.audience);
     }
     result += `Amount owed is ${format(totalAmount / 100)}\n`;
     result += `You earned ${volumeCredits} credits\n`;
