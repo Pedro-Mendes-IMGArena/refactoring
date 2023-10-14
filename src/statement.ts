19ad7b2b25473acd70e6299650b60c62af63649a
Pedro Mendes <108732350+Pedro-Mendes-IMGArena@users.noreply.github.com>
2023-10-14 10:54:37 +0100

    refactor: move format function

--- src/statement.ts
+++ src/statement.ts
@@ -18,20 +18,15 @@ type PlaysMap = Record<string, Play>;
 
 export function statement(invoice: Invoice, plays: PlaysMap) {
     let result = `Statement for ${invoice.customer}\n`;
-    const format = new Intl.NumberFormat("en-US",
-        {
-            style: "currency", currency: "USD",
-            minimumFractionDigits: 2
-        }).format;
-
+    
     for (let perf of invoice.performances) {
         const play: Play = plays[perf.playID];
         // print line for this order
-        result += ` ${play.name}: ${format(calculateAmountOwedBy(play.type, perf.audience) / 100)} (${perf.audience} seats)\n`;
+        result += ` ${play.name}: ${formatMoney(calculateAmountOwedBy(play.type, perf.audience) / 100)} (${perf.audience} seats)\n`;
     }
    
     
-    result += `Amount owed is ${format(calculateTotalAmountOwed(invoice.performances, plays) / 100)}\n`;
+    result += `Amount owed is ${formatMoney(calculateTotalAmountOwed(invoice.performances, plays) / 100)}\n`;
     result += `You earned ${calculateVolumeCredits(invoice.performances, plays)} credits\n`;
     return result;
 }
@@ -77,4 +72,13 @@ function calculateTotalAmountOwed(performances: Invoice['performances'], plays: 
                 throw new Error(`unknown type: ${playType}`);
         }
         return result;
+ }
+    
+ function formatMoney(money: number) {
+        return new Intl.NumberFormat("en-US",
+            {
+                style: "currency", currency: "USD",
+                minimumFractionDigits: 2
+            }).format(money);
+    
     }
